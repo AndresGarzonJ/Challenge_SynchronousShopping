964c07903899dcd0d817e4eab8f5344f442b1225
AndresGarzonJ <sebasgarzonj@gmail.com>
2023-03-15 12:01:23 -0500

    DEBUG = False

--- synchronous_shopping/synchronous_shopping/settings.py
+++ synchronous_shopping/synchronous_shopping/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'me2jry05q2dmi6b(1o^3-ixri1*ph-j3ed*_*za(a+yq2y_&4*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
